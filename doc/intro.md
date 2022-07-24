5d68a3a5b048f6be3db6b7482c9025246368b1d9
Andrew Baxter <rhinocratic@googlemail.com>
2022-07-24 10:34:10 +0100

    Blank project

---
+++ doc/intro.md
@@ -0,0 +1,3 @@
+# Introduction to rhinocratic/inventory
+
+TODO: write [great documentation](http://jacobian.org/writing/what-to-write/)
---
+++ test/rhinocratic/inventory_test.clj
@@ -0,0 +1,7 @@
+(ns rhinocratic.inventory-test
+  (:require [clojure.test :refer :all]
+            [rhinocratic.inventory :refer :all]))
+
+(deftest a-test
+  (testing "FIXME, I fail."
+    (is (= 0 1))))
