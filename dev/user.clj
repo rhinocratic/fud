0cc4480fa4c871843abcf01dd09667ecf626dd14
Andrew Baxter <rhinocratic@googlemail.com>
2022-07-31 18:51:43 +0100

    Ignore some unused public vars

--- dev/user.clj
+++ dev/user.clj
@@ -10,26 +10,31 @@
 
 ;;;; REPL convenience functions
 
+#_{:clj-kondo/ignore [:clojure-lsp/unused-public-var]}
 (defn go
   "Prepare configuration and start the system services with Integrant-repl"
   ([] (go :dev))
   ([profile] 
    (environment-prep! profile) (ig-repl/go)))
 
+#_{:clj-kondo/ignore [:clojure-lsp/unused-public-var]}
 (defn reset
   "Read updates from the configuration and restart the system services with Integrant-repl"
   ([] (reset :dev))
   ([profile] 
    (environment-prep! profile) (ig-repl/reset)))
 
+#_{:clj-kondo/ignore [:clojure-lsp/unused-public-var]}
 (defn halt
   "Shut down all services"
   [] (ig-repl/halt))
 
+#_{:clj-kondo/ignore [:clojure-lsp/unused-public-var]}
 (defn system
   "The initialised integrant system configuration"
   [] ig-state/system)
 
+#_{:clj-kondo/ignore [:clojure-lsp/unused-public-var]}
 (defn config
   "The static (uninitialised) integrant system configuration"
   [] ig-state/config)
--- src/clj/rhinocratic/fud/db/connection.clj
+++ src/clj/rhinocratic/fud/db/connection.clj
@@ -4,10 +4,10 @@
    [next.jdbc.connection :as connection])
   (:import (com.zaxxer.hikari HikariDataSource)))
 
-(defn ^HikariDataSource pooled-datasource
+(defn pooled-datasource
   "Create a JDBC DataSource from a map containing a DB spec."
-  [datasource]
+  ^HikariDataSource [datasource]
   (let [datasource (connection/->pool com.zaxxer.hikari.HikariDataSource datasource)]
-    (.close (jdbc/get-connection datasource))
+    (.close (jdbc/get-connection datasource)) 
     datasource))
 
