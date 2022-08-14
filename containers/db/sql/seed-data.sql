947db164b8e793b4891f1b4e06a703a3f6e1854a
Andrew Baxter <rhinocratic@googlemail.com>
2022-08-14 15:14:30 +0100

    Added seed data file

---
+++ containers/db/sql/seed-data.sql
@@ -0,0 +1,4 @@
+INSERT INTO fud.supplier(supplier_name, website, email, telephone, address, notes)
+VALUES
+('Sainsbury''s', 'https://www.sainsburys.co.uk/shop/MobileHomeView?langId=44&storeId=10151', 'someone@sainsburys.co.uk', '0800 328 1700', '33 Holborn, London, EC1N 2HT', 'Not what it was'),
+('The Italian Shop', 'https://italianfoodshop.co.uk/','info@italianfoodshop.co.uk', '01234 211036', 'Unit 11, 18 The Harpur Centre, Bedford MK40 1TP, UK', 'Mostly decent stuff');
